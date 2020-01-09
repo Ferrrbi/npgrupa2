0602629bc12cd21dfb739c17ddc01d8ba3896a24
Filip <filip.gorczyca141@gmail.com>
2020-01-09 17:40:06 +0100

    Funkcja dodawania i switch w main

--- gr2/main.cpp
+++ gr2/main.cpp
@@ -3,12 +3,33 @@
 using namespace std;
 int x;
 int y;
+
 int main()
 {
+int wybor;
+extern void dodawanie();
+
+
     cout << "Podaj liczbe pierwsza: ";
     cin >> x;
     cout << "Podaj liczbe druga: ";
     cin >> y;
     cout<<"Podale liczbe "<< x << " oraz " << y << endl;
+
+
+    cout<<"wybierz operacje"<<endl;
+    cout<<"Wcisnij 1 aby dodać"<<endl;
+    cout<<"Wcisnij 2 aby odjac"<<endl;
+    cout<<"Wcisnij 3 aby pomnozyc"<<endl;
+    cout<<"Wcisnij 4 aby podzielic"<<endl;
+    cin>>wybor;
+    switch(wybor)
+    {
+        case 1:dodawanie();
+
+
+
+    }
+
         return 0;
 }
