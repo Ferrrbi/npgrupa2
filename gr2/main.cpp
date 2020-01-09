ac72e2fa5cea22780dd98b78907a6d29441c9260
Filip <filip.gorczyca141@gmail.com>
2020-01-09 13:25:59 +0100

    Plik main

---
+++ gr2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int x;
+int main()
+{
+    cout << "Podaj liczbe:" << endl;
+    cin>>x;
+    cout<<"  liczba po szeregu operacji"<<x<<endl;
+        return 0;
+}
